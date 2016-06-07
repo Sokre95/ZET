e4a79bae742415e7ca96752d6a1c0142938ba404
Martin Pisacic <martin.pisacic@fer.hr>
2016-06-07 15:19:23 +0200

    Bug fixes

--- app/src/main/java/fer/ruazosa/ruazosa16_zet/activities/MainActivity.java
+++ app/src/main/java/fer/ruazosa/ruazosa16_zet/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package fer.ruazosa.ruazosa16_zet.activities;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -20,6 +22,10 @@ import fer.ruazosa.ruazosa16_zet.tram.TramFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String HOME_FRAGMENT = "home";
+    public static final String BUS_FRAGMENT = "bus";
+    public static final String TRAM_FRAGMENT = "tram";
+
     @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
     @BindView(R.id.navigation_view) NavigationView navigationView;
@@ -33,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-
         setSupportActionBar(toolbar);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,
@@ -42,9 +47,18 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
         fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.main_container, new HomeFragment());
+        fragmentTransaction.add(R.id.main_container, new HomeFragment(), HOME_FRAGMENT);
         fragmentTransaction.commit();
-        getSupportActionBar().setTitle("Home");
+
+        if(savedInstanceState != null) {
+            getSupportActionBar().setTitle(savedInstanceState.getCharSequence("title"));
+            Fragment currentFragment = getSupportFragmentManager().getFragment(savedInstanceState, "fragment");
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.main_container, currentFragment);
+            fragmentTransaction.commit();
+        } else {
+            getSupportActionBar().setTitle("Home");
+        }
 
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
@@ -54,15 +68,15 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.home_item:
                         getSupportActionBar().setTitle("Home");
-                        fragmentTransaction.replace(R.id.main_container, new HomeFragment());
+                        fragmentTransaction.replace(R.id.main_container, new HomeFragment(), HOME_FRAGMENT);
                         break;
                     case R.id.bus_item:
                         getSupportActionBar().setTitle("Bus");
-                        fragmentTransaction.replace(R.id.main_container, new BusFragment());
+                        fragmentTransaction.replace(R.id.main_container, new BusFragment(), BUS_FRAGMENT);
                         break;
                     case R.id.tram_item:
                         getSupportActionBar().setTitle("Tram");
-                        fragmentTransaction.replace(R.id.main_container, new TramFragment());
+                        fragmentTransaction.replace(R.id.main_container, new TramFragment(), TRAM_FRAGMENT);
                         break;
                     default:
                         break;
@@ -80,5 +94,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        CharSequence title = getSupportActionBar().getTitle();
+        outState.putCharSequence("title", title);
+        Fragment currentFragment = getSupportFragmentManager().findFragmentByTag(HOME_FRAGMENT);
+        if(currentFragment == null) {
+            currentFragment = getSupportFragmentManager().findFragmentByTag(BUS_FRAGMENT);
+            if(currentFragment == null) {
+                currentFragment = getSupportFragmentManager().findFragmentByTag(TRAM_FRAGMENT);
+            }
+        }
+        getSupportFragmentManager().putFragment(outState, "fragment", currentFragment);
     }
 }
