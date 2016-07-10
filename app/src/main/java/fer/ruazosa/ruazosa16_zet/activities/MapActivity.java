a4880d8b2d763a1b5c5afd2249bd2349e891ff54
Luka Kovačić <luka.kovacic2@fer.hr>
2016-07-10 12:38:48 +0200

    nes me j...

--- app/src/main/java/fer/ruazosa/ruazosa16_zet/activities/MapActivity.java
+++ app/src/main/java/fer/ruazosa/ruazosa16_zet/activities/MapActivity.java
@@ -1,20 +1,28 @@
 package fer.ruazosa.ruazosa16_zet.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
 
 import fer.ruazosa.ruazosa16_zet.R;
+import fer.ruazosa.ruazosa16_zet.ZetWebService;
+import fer.ruazosa.ruazosa16_zet.model.Line;
+import fer.ruazosa.ruazosa16_zet.model.Station;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Line line;
+    private ZetWebService zetWebService;
+    private List<Station> stationList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,11 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        zetWebService = ZetWebService.getInstance();
+        line = new Line(203);
+        zetWebService.loadLine(line).subscribe();
+        stationList.addAll(line.getStations());
     }
 
 
@@ -40,9 +53,17 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.width(5).color(Color.BLUE);
+        for(Station station : stationList){
+            polylineOptions.add(new LatLng(station.getLatitude(),station.getLongitude()));
+        }
+        mMap.addPolyline(polylineOptions);
+
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
